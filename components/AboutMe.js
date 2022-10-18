23c3955c25b48d3a3fa0f1537b39502e01e4e0cf
Ella Svete <ella.svete@gmail.com>
2022-10-18 14:08:17 -0700

    userdata added

---
+++ components/AboutMe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import UserData from '@constants/data';
+
+export default function AboutMe() {
+  return (
+    <section className="bg-white dark:bg-gray-800">
+      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
+        <h1>
+          About Me.
+        </h1>
+      </div>
+      
+    </section>
+  )
+}
---
+++ constants/data.js
@@ -0,0 +1,62 @@
+const userData = {
+  githubUserName: 'EllaSvete',
+  name: 'Ella Svete',
+  designation: 'Full-Stack Developer',
+  avatarUrl: '/avatar.png',
+  email: 'ella.svete@gmail.com',
+  address: 'Olympia, WA',
+  projects: [
+    {
+      title: "Portfolio",
+      link: "https://ellasveteportfolio",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Smells Like Devs Cooking",
+      link: "https://smells-like-devs-cooking-frontend-rho.vercel.app/",
+      imgUrl: "/devscooking.png",
+    },
+    {
+      title: 'Record Bin',
+      link: 'https://reccrdbin.netlify.app/',
+      imgUrl: '/recordbin.png',
+    },
+  ],
+  about: {
+    title:
+    "I'm a software developer and craftsperson who loves building things an problem solving solutions.",
+    description: [
+      "I've been developing full-stack applications for nearly a year now. I have a background in music and jewelry design and I have been inspired by finding the parallels between my background and the tech industry."
+    ]
+  },
+  experience: [
+    {
+      title: "Owner",
+      company: 'E. Svete Collections',
+      year:'2019-Present',
+      desc: "Improves soldering and finishing skills, boosting efficiency by 15% monthly. Designs and manufactures an average of 15-20 quality pieces a day."
+    },
+    {
+      title: "Band Manager",
+      company: "Table Sugar",
+      year: "2014-2022",
+      companyLink: "https://tablesugarband.bandcamp.com/",
+      desc:"Coordinates logistics via Google Sheets ensuring profitable tours. Mediates team dynamics resulting in creative success as a band."
+    },
+    {
+      title: "Co-Retail Manager",
+      company: "Psychic Sister",
+      year: "2020-2022",
+      companyLink: "https://www.psychic-sister.com/",
+      desc: "Supervised team of seven producing high-volume output per clientele standards. Streamlined logistics with owners and auxiliary locations via clear communication."
+    },
+  ],
+  resumeUrl:
+  "https://docs.google.com/document/d/1eRZCjVwB5tOfTOoDXAgK8pDkTpueBhnnxKsSyIG2vaM/edit?usp=sharing",
+  socialLinks: {
+    linkedIn: "https://www.linkedin.com/in/ellasvete/",
+    github: "https://github.com/EllaSvete",
+  },
+};
+
+export default userData;
